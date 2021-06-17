049f53793e7fb3fe665de3b4f2cf6af5f299f879
Jef Rowley <jef.rowley@gmail.com>
2021-06-16 23:18:52 -0500

    middleware to ensure user can access property info

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,24 +1,25 @@
 const reservationController = require('../controllers/reservation');
+const propUser = require('../middleware/is-property-user');
 const express = require('express');
 const router = express.Router();
 
-//get by reservation id
-router.get('/:reservationId', reservationController.getReservation)
-
-//get all reservations for a single property
-router.get('/list/:propertyId', reservationController.getReservations);
-
-//post a new reservation
-router.post('/:propertyId', reservationController.postReservation);
-
-//update an existing reservation (date and/or comments)
-router.put('/:reservationId', reservationController.modifyReservation);
-
-//update the approval status of a reservation request
-router.put('/approval/:reservationId', reservationController.approveReservation);
+//get by reservation id (in query)
+router.get('/:propertyId/:reservationId', propUser, reservationController.getReservation)
 
 //remove a reservation from the system
-router.delete('/:reservationId', reservationController.deleteReservation);
+router.delete('/:propertyId/:reservationId', propUser, reservationController.deleteReservation);
+
+//get all reservations for a single property
+router.get('/list/:propertyId', propUser, reservationController.getReservations);
+
+//post a new reservation
+router.post('/:propertyId', propUser, reservationController.postReservation);
+
+//update an existing reservation (date and/or comments)
+router.put('/:propertyId/:reservationId', propUser, reservationController.modifyReservation);
+
+//update the approval status of a reservation request
+router.put('/:propertyId/approval/:reservationId', propUser, reservationController.approveReservation);
 
 //get all reservations for a single user
 router.get('/', reservationController.getUserReservations);
