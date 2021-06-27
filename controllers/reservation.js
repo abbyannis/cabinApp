c71cdb42df15d6d94d117a2b3163cb8e50c8de36
Jef Rowley <jef.rowley@gmail.com>
2021-06-26 22:10:03 -0500

    Tested reservation controller

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -1,28 +1,15 @@
 const Reservation = require('../models/reservation');
-const Property = require('../models/property');
-const User = require('../models/user');
 const utils = require('../util/utilities');
 const { validationResult } = require("express-validator");
+const nodemailer = require('nodemailer');
 
-//gets a reservation by specified id in the req params (req.params.reservationId)
-function getReservationById(req) {  
-  return Reservation     
-    .findById(req.params.reservationId)
-    .then(reservation => {
-      if (!reservation) {
-        const err = new Error('Reservation not found');
-        err.statusCode = 404;
-        throw error;
-      }            
-      return reservation;
-    });    
-}
-
-//checks if specified in/out dates are valid based on existing reservations
-//returns true if they are, false if not.
-function checkDates(inDate, outDate, propertyId) {
-
-}
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_ACCOUNT, 
+    pass: process.env.EMAIL_PWD
+  }
+});
 
 //find all reservations for the specified property (id in params)
 //with a start date <= qeury.endDate 
@@ -64,7 +51,7 @@ exports.getReservations = (req, res, next) => {
 
 //get reservationby a specific reservationId (in params)
 exports.getReservation = (req, res, next) => {        
-  getReservationById(req)  
+  Reservation.getReservationById(req.params.reservationId)  
   .then(reservation => {          
       res.status(200).json({ reservation });          
   })    
@@ -97,24 +84,31 @@ exports.postReservation = (req, res, next) => {
     return res.status(422).json( { errors }); 
   } 
   //check if dates are valid (not reserved and shorter max length but longer than min) in validation.
-      
-  //if not valid, return error
-      
-  //if valid, create:
-  let reservation = new Reservation({
-    user: req.userId,
-    property: req.params.propertyId,
-    comments: req.body.comments,
-    status: "pending",
-    startDate: new Date(req.body.startDate),
-    endDate: new Date(req.body.endDate)
-  });
-  reservation
-    .save()
-    .then(result => {
-      return res.status(201).json({reservation: result});
-    })
-    .catch(err => {
+  const startDate = new Date(req.body.startDate);
+  const endDate = new Date(req.body.endDate);
+  const user = req.userId;
+  const property = req.params.propertyId;  
+  Reservation.CheckDateAvailability(startDate, endDate, property)
+  .then(availability => {    
+    //if valid, create:
+    if (!availability) {
+      return res.status(409).json({ reservation: null, message: "No availability during selected time." });
+    }     
+    let reservation = new Reservation({
+      user: user,
+      property: property,
+      comments: req.body.comments,
+      status: "pending",
+      startDate: startDate,
+      endDate: endDate
+    });    
+    reservation    
+      .save()
+      .then(result => {
+        return res.status(201).json({ reservation: result, message: "Reservation submitted." });
+      });
+    })          
+    .catch(err => {      
       if (!err.statusCode) err.statusCode = 500;
       next(err);    
     });
@@ -126,45 +120,64 @@ exports.modifyReservation = (req, res, next) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(422).json( { errors });
-  } 
-  getReservationById(req)
-  .then(reservation => {
-      if(reservation.user.toString() !== req.userId.toString()) {        
-        const error = new Error("Unauthorized attempt to modify a reservation.");
-        error.statusCode = 401;
-        throw error;
-      }
-      //check if dates are valid (not reserved and shorter max length but longer than min) in validation.
-      
-      //if not valid, return error
-      
-      //if valid, update:
-      reservation.startDate = req.body.startDate;
-      reservation.endDate = req.body.startDate;
+  }   
+  const startDate = new Date(req.body.startDate);
+  const endDate = new Date(req.body.endDate);
+  const user = req.userId;
+  const property = req.params.propertyId;     
+  Reservation.getReservationById(req.params.reservationId) 
+  .then(reservation => {    
+    if(reservation.user.toString() !== user) {             
+      const error = new Error("Unauthorized attempt to modify a reservation.");
+      error.statusCode = 401;
+      throw error;
+    }     
+    //check if dates are valid (not reserved and shorter max length but longer than min) in validation.
+    Reservation.CheckDateAvailability(startDate, endDate, property, user)
+    .then(availability => {    
+      //if valid, create:
+      console.log(availability);    
+      if (!availability) {
+        return res.status(409).json({ reservation: null, message: "No availability during selected time." });
+      }       
+      //if valid, update:      
+      reservation.startDate = startDate;
+      reservation.endDate = endDate;
       reservation.comments = req.body.comments;
       reservation.status = 'pending';
       return reservation.save();
-    })
+    })    
     .then(result => {
       res.status(200).json({ 
         message: 'Reservation has been modified.', 
         reservation: result
       });
-    })
-    .catch(err => {
-      if (!err.statusCode) err.statusCode = 500;
-      next(err);
-    });
+    }); 
+  })     
+  .catch(err => {    
+    if (!err.statusCode) err.statusCode = 500;
+    next(err);
+  });
 };
 
 //Approves or rejects(and deletes) a reservation based on the reservationId in the params
-exports.approveReservation = (req, res, next) => {  
-   getReservationById(req)
+exports.approveReservation = (req, res, next) => {    
+  const status = req.body.status;
+  let myReservation;
+  Reservation.findById(req.params.reservationId)
+    .populate('user')
+    .populate('property')
     .then(reservation => {
-      if (req.body.status === 'confirmed') {
-        reservation.status = req.body.status;
+      if(!reservation) {
+        const err = new Error('Reservation not found');
+        err.statusCode = 404;
+        throw error;
+      }
+      myReservation = reservation;         
+      if (status === 'confirmed') {
+        reservation.status = status;        
         return reservation.save();
-      } else if (req.body.status === 'rejected') {
+      } else if (req.body.status === 'declined') {
         return Reservation.findByIdAndRemove(req.params.reservationId);
       } else {
         const error = new Error("Invalid reservation status received.");
@@ -173,10 +186,18 @@ exports.approveReservation = (req, res, next) => {
       } 
     })    
     .then(result => {
-      //send result
+      //send result      
       res.status(200).json({ 
-        message: `Reservation has been ${req.body.status}.`, 
+        message: `Your reservation has been ${status}.`, 
         reservation: result
+      });
+      //notify user of status
+      transporter.sendMail({
+        to: myReservation.user.email,
+        from: 'reservations@atTheCabin.com',
+        subject: `Your reservation request has been ${status}`,
+        html: `<p>Your reservation request for property ${myReservation.property.name}, ${myReservation.property.location} has been ${status} for the following dates:
+        ${myReservation.startDate.toLocaleDateString()} to ${myReservation.endDate.toLocaleDateString()}.`        
       });
     })    
     .catch(err => {
@@ -186,20 +207,22 @@ exports.approveReservation = (req, res, next) => {
 };
 
 //Removes a reservation from the table based on the reservationId in the params
-exports.deleteReservation = (req, res, next) => {
-  getReservationById(req)
-  .then(reservation => {
-    if(reservation.user.toString() !== req.userId.toString()) {        
-      const error = new Error("Unauthorized attempt to modify a reservation.");
-      error.statusCode = 401;
-      throw error;
-    }
+exports.deleteReservation = (req, res, next) => {   
+  Reservation.getReservationById(req.params.reservationId) 
+  .then(reservation => {     
+    if(reservation.user.toString() !== req.userId.toString()) {     
+      console.log("h1");   
+      const err = new Error("Unauthorized attempt to modify a reservation.");
+      err.statusCode = 401;
+      throw err;
+    }     
     return Reservation.findByIdAndRemove(req.params.reservationId);
   })
-  .then(result => {
+  .then(result => {    
     res.status(200).json({ message: 'Reservation has been canceled.'});
   })
   .catch(err => {
+    console.log(err);
     if (!err.statusCode) err.statusCode = 500;
     next(err);
   });
--- routes/reservation.js
+++ routes/reservation.js
@@ -8,11 +8,11 @@ const router = express.Router();
 //get by reservation id (in query)
 router.get('/:propertyId/:reservationId', propUser, reservationController.getReservation)
 
-//remove a reservation from the system
-router.delete('/:propertyId/:reservationId', propUser, reservationController.deleteReservation);
-
 //get all reservations for a single property
-router.get('/list/:propertyId', propUser, reservationController.getReservations);
+router.get('/:propertyId', propUser, reservationController.getReservations);
+
+//get all reservations for a single user
+router.get('/', reservationController.getUserReservations);
 
 //post a new reservation
 router.post('/:propertyId', propUser,
@@ -57,7 +57,7 @@ router.patch('/:propertyId/:reservationId', propUser,
 //update the approval status of a reservation request
 router.patch('/:propertyId/approval/:reservationId', propUser, reservationController.approveReservation);
 
-//get all reservations for a single user
-router.get('/', reservationController.getUserReservations);
+//remove a reservation from the system
+router.delete('/:propertyId/:reservationId', propUser, reservationController.deleteReservation);
 
 module.exports = router;
